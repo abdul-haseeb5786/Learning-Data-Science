31c3106840ea767502f658d76f79dd728da45353
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-28 14:42:37 +0500

    Update Day1.py

--- NumPy/Day1/Day1.py
+++ NumPy/Day1/Day1.py
@@ -1,1 +1,18 @@
+# Day 1 - NumPy Basics
+import numpy as np
+import sys
+import time
+
+# --------------------------
+# 1. Continuous Memory (Compact Storage)
+# --------------------------
+py_list = [1, 2, 3, 4, 5]
+np_array = np.array([1, 2, 3, 4, 5])
+
+print("Python list element addresses:")
+for x in py_list:
+    print(id(x))
+
+print("\nNumPy array element addresses:")
+print(np_array.__array_interface__['data'])
 
