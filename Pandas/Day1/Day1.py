c2cfe159e141d50e831922932db659537f8dd478
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-11-01 15:02:43 +0500

    Update Day1.py

--- Pandas/Day1/Day1.py
+++ Pandas/Day1/Day1.py
@@ -4,3 +4,10 @@
 
 import pandas as pd
 
+# -------------------------------------------------------------
+# 📘 1. Creating a Series
+# -------------------------------------------------------------
+
+# From a list
+marks = pd.Series([85, 90, 78, 92, 88])
+print("📄 Series from List:\n", marks)
